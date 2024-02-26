cb1d5e5afbb7b7f4098c852ff44800e09c905fb8
Niamh <niamh.linguist@gmail.com>
2024-02-26 12:17:40 +0200

    seeded users table with stater data and added photos

--- model/init_db.sql
+++ model/init_db.sql
@@ -18,24 +18,32 @@ CREATE TABLE `users`(
     `photoSource` VARCHAR(40) NULL
 )ENGINE=INNODB AUTO_INCREMENT = 10000;
 
-CREATE TABLE `families`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `nickname` VARCHAR(40) NOT NULL,
-    `adminUser` BIGINT NOT NULL
-)ENGINE=INNODB AUTO_INCREMENT = 1000;
+INSERT INTO users(firstName, lastName, email, password, phoneNumber, dob, sex, gender, pronouns, photoSource)
+VALUES
+    ('Sofie', 'Stephens', 'sofia@email.com', 'qwerty', '123456789',  19900120, '1', '1', '1', 'stephens-sofia'),
+    ('Claus', 'Weismann', 'claus@email.com', 'qwerty', '123456789', 19920212, '0', '0', '0', 'weismann-claus'),
+    ('Gale', 'Vasquez', 'gale@email.com', 'qwerty', '123456789', 19890123, '1', '2', '2', 'vasquez-gale');
 
-CREATE TABLE `children`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `firstName` VARCHAR(40) NOT NULL,
-    `lastName` VARCHAR(40) NOT NULL,
-    `sex` CHAR(2) NOT NULL DEFAULT '0',
-    `gender` CHAR(3) NOT NULL DEFAULT '0',
-    `pronouns` CHAR(3) NOT NULL DEFAULT '0',
-    `primaryFamily` BIGINT NOT NULL,
-    `primaryGuardian` BIGINT,
-    `familyDoctor` BIGINT,
-    `teacher` BIGINT,
-    `dob` DATE NOT NULL
-)ENGINE=INNODB AUTO_INCREMENT = 200000;
+
+
+-- CREATE TABLE `families`(
+--     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     `nickname` VARCHAR(40) NOT NULL,
+--     `adminUser` BIGINT NOT NULL
+-- )ENGINE=INNODB AUTO_INCREMENT = 1000;
+
+-- CREATE TABLE `children`(
+--     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     `firstName` VARCHAR(40) NOT NULL,
+--     `lastName` VARCHAR(40) NOT NULL,
+--     `sex` CHAR(2) NOT NULL DEFAULT '0',
+--     `gender` CHAR(3) NOT NULL DEFAULT '0',
+--     `pronouns` CHAR(3) NOT NULL DEFAULT '0',
+--     `primaryFamily` BIGINT NOT NULL,
+--     `primaryGuardian` BIGINT,
+--     `familyDoctor` BIGINT,
+--     `teacher` BIGINT,
+--     `dob` DATE NOT NULL
+-- )ENGINE=INNODB AUTO_INCREMENT = 200000;
 
 
