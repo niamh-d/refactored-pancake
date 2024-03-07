b5d5bddfe2a19518d67fa0a247b40606abee9ea1
Niamh <niamh.linguist@gmail.com>
2024-03-07 14:16:50 +0200

    finished styling doctor card

--- client/src/components/DoctorCard.jsx
+++ client/src/components/DoctorCard.jsx
@@ -4,6 +4,7 @@ import Tooltip from "@mui/material/Tooltip";
 import LanguageIcon from "@mui/icons-material/Language";
 import ChatIcon from "@mui/icons-material/Chat";
 import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 
 function renderLanguageFlags(languages) {
   // const renderEmoji = (lang) => {
@@ -35,21 +36,42 @@ function renderLanguageFlags(languages) {
 }
 
 const DoctorCard = ({ doctor }) => {
-  const { firstName, lastName, clinicName, city, languages } = doctor;
+  const {
+    firstName,
+    lastName,
+    clinicName,
+    city,
+    languages,
+    streetAddress,
+    clinicPhoneNo,
+    websiteURL,
+  } = doctor;
 
   const languagesArr = renderLanguageFlags(languages);
 
   return (
     <div className="mt-10 p-5 rounded-lg ml-auto mr-auto w-96 shadow-lg">
       <div className="mt-5 p-5">
-        <ul className="flex flex-col text-xl gap-2">
+        <ul className="flex flex-col text-xl gap-3">
           <li className="pt-3 pb-3">
             <span className="font-bold text-3xl uppercase tracking-wider">
               {lastName}
             </span>
             <span className="text-2xl tracking-wider">, {firstName}</span>
           </li>
-          <li className="flex gap-3 justify-self-center">
+          <li className="flex gap-3 justify-self-center mt-3">
+            <Tooltip title="Languages" placement="left">
+              <ChatIcon />
+            </Tooltip>
+            <div className="flex gap-3 justify-self-center">
+              {languagesArr.map((lang, i) => (
+                <span key={i} className="text-lg">
+                  {lang}
+                </span>
+              ))}
+            </div>
+          </li>
+          <li className="flex gap-3 justify-self-center font-semibold">
             {" "}
             <Tooltip title="Clinic" placement="left">
               <LocalHospitalIcon />
@@ -57,22 +79,26 @@ const DoctorCard = ({ doctor }) => {
             {clinicName}
           </li>
           <li className="flex gap-3 justify-self-center">
-            <Tooltip title="City" placement="left">
+            <Tooltip title="Address" placement="left">
               <LocationCityIcon />
             </Tooltip>
-            <span>{city}</span>
+            <span>{`${city}, ${streetAddress}`}</span>
+          </li>
+          <li className="flex gap-3 justify-self-center">
+            <Tooltip title="Website" placement="left">
+              <LanguageIcon />
+            </Tooltip>
+            <a target="_blank" href={websiteURL}>
+              Open website
+            </a>
+          </li>
+          <li className="flex gap-3 justify-self-center">
+            <Tooltip title="Phone no." placement="left">
+              <LocalPhoneIcon />
+            </Tooltip>
+            <a href={`tel:${clinicPhoneNo}`}>{clinicPhoneNo}</a>
           </li>
         </ul>
-        <div className="flex gap-3 justify-self-center mt-3">
-          <Tooltip title="Languages" placement="left">
-            <ChatIcon />
-          </Tooltip>
-          <div className="flex gap-3 justify-self-center">
-            {languagesArr.map((lang) => (
-              <span key={lang}>{lang}</span>
-            ))}
-          </div>
-        </div>
       </div>
     </div>
   );
