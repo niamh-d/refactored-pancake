17c59eb9f345fb2f6fcba0f3b8bc0091d283bdf7
Niamh <niamh.linguist@gmail.com>
2024-03-07 16:47:21 +0200

    added id to join query

---
+++ routes/education.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var router = express.Router();
+const db = require("../model/helper");
+
+router.get("/", async function (req, res, next) {
+  try {
+    const id = req.query.familyId;
+
+    const results = await db(
+      `SELECT schoolName, schoolEmail, schoolPhoneNo, streetAddress, city, websiteURL, t.id, firstName, lastName, mobileNo, email FROM schools AS s INNER JOIN teachers AS t ON s.id = t.schoolId WHERE t.family = ${id};`
+    );
+
+    res.send(results.data);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+module.exports = router;
